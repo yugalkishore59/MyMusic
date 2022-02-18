76ef2e1795fe51df6ca89235f09ecf1e0e06048f
Yugal Kishore <yugalkishore59@gmail.com>
2022-02-19 00:42:16 +0530

    shared preference for ArrayList<File>

--- app/src/main/java/com/fetchhere/mymusic/MainActivity.java
+++ app/src/main/java/com/fetchhere/mymusic/MainActivity.java
@@ -10,20 +10,32 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 import com.fetchhere.mymusic.fragments.all_songs_fragment;
 import com.fetchhere.mymusic.fragments.favourite_fragment;
 import com.fetchhere.mymusic.fragments.now_playing_fragment;
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
     private TabLayout tab_layout;
     private ViewPager view_pager;
+
+    ArrayList<File> allSongs;
+
+    private static final String LIST_KEY = "all songs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +83,60 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public static void writeArrayListInPref(Context context, ArrayList<File> list) {
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(list);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(LIST_KEY, jsonString);
+        editor.apply();
+    }
+
+    public static ArrayList<File> readListFromPref(Context context) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        String jsonString = pref.getString(LIST_KEY, "");
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<File>>() {}.getType();
+        ArrayList<File> list = gson.fromJson(jsonString, type);
+        return list;
+    }
+
+    public ArrayList<File> findSong(File rootFolder) {
+
+        //ArrayList to store all songs
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = rootFolder.listFiles();
+
+        for (File singleFile : files) {
+
+            //Adding the directory to arrayList if it is not hidden
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+
+                arrayList.addAll(findSong(singleFile));
+
+            } else {
+                //Adding the single music file to ArrayList
+                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+
+        return arrayList;
+    }
+
+    void scanMusic(){
+        File sdfile = new File("/storage/3263-3464");
+        allSongs=findSong(sdfile);
+        writeArrayListInPref(this,allSongs);
+    }
+
+    void listSongs(){
+        
+    }
+
     void continueActivity() {
 
         view_page_adapter adapter = new view_page_adapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
@@ -79,9 +145,13 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragments(new favourite_fragment(), "Favourites");
         view_pager.setAdapter(adapter);
 
-        scanMusic();
-    }
-    void scanMusic(){
+        allSongs = readListFromPref(this);
+
+        if (allSongs == null)
+            scanMusic();
+
+        listSongs();
 
     }
+
 }
