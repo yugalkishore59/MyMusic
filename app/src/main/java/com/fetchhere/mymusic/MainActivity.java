631b918bb6ea4c94da3dc6785ff2675b275cc98b
Yugal Kishore <yugalkishore59@gmail.com>
2022-03-12 16:16:06 +0530

    queue and improvements in shared preferences
    made new list of files "queue". now most of the operations will be done on queue rather than allSongs.
    
    some improvements in shared preferences

--- app/src/main/java/com/fetchhere/mymusic/MainActivity.java
+++ app/src/main/java/com/fetchhere/mymusic/MainActivity.java
@@ -13,8 +13,6 @@ import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.TextView;
 
 import com.fetchhere.mymusic.fragments.all_songs_fragment;
 import com.fetchhere.mymusic.fragments.favourite_fragment;
@@ -34,10 +32,15 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager view_pager;
     public view_page_adapter adapter;
 
-    ArrayList<File> allSongs;
+    ArrayList<File> allSongs,queue;
 
-
-    private static final String LIST_KEY = "all songs";
+    SharedPreferences sharedPreferencesVariables;
+    SharedPreferences.Editor editor;
+    public static final String SHARED_PREF_KEY = "shared Preferences Variables";
+    public static final String SORT_KEY = "current sort";
+    public static final String CURR_SONG_KEY = "current Song Index";
+    public static final String LIST_KEY = "all songs";
+    public static final String QUEUE_KEY = "queue";
 
 
     @Override
@@ -48,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
         view_pager = findViewById(R.id.view_pager);
 
         tab_layout.setupWithViewPager(view_pager);
+
+        sharedPreferencesVariables=this.getSharedPreferences(SHARED_PREF_KEY, Context.MODE_PRIVATE);
+        editor = sharedPreferencesVariables.edit();
 
         checkForPermission();
 
@@ -86,19 +92,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static void writeArrayListInPref(Context context, ArrayList<File> list) {
+    public static void writeArrayListInPref(Context context, ArrayList<File> list, String key) {
         Gson gson = new Gson();
         String jsonString = gson.toJson(list);
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = pref.edit();
-        editor.putString(LIST_KEY, jsonString);
+        editor.putString(key, jsonString);
         editor.apply();
     }
 
-    public static ArrayList<File> readListFromPref(Context context) {
+    public static ArrayList<File> readListFromPref(Context context,String key) {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
-        String jsonString = pref.getString(LIST_KEY, "");
+        String jsonString = pref.getString(key, "");
 
         Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<File>>() {}.getType();
@@ -142,30 +148,38 @@ public class MainActivity extends AppCompatActivity {
     void scanMusic(){
 
 
-        File[] sdfile = ContextCompat.getExternalFilesDirs(getApplicationContext(),null);
-        for (int i=0;i< sdfile.length;i++)
+        File[] fileDirectory = ContextCompat.getExternalFilesDirs(getApplicationContext(),null);
+        for (int i = 0; i< fileDirectory.length; i++)
         {
-            String path = sdfile[i].getParent().replace("/Android/data/","").replace(getPackageName(),"");
-            sdfile[i]= new File(path);
+            String path = fileDirectory[i].getParent().replace("/Android/data/","").replace(getPackageName(),"");
+            fileDirectory[i]= new File(path);
         }
 
-        allSongs=findSong(sdfile[0]);
-        for(int i=1;i<sdfile.length;i++)
-        allSongs.addAll(findSong(sdfile[i]));
+        allSongs=findSong(fileDirectory[0]);
+        for(int i = 1; i< fileDirectory.length; i++)
+        allSongs.addAll(findSong(fileDirectory[i]));
 
-        writeArrayListInPref(this,allSongs);
+        writeArrayListInPref(this,allSongs,LIST_KEY);
+        editor.putInt(CURR_SONG_KEY, 0);
+        editor.putInt(SORT_KEY, 0);
+        editor.commit();
 
     }
 
     void continueActivity() {
 
-        allSongs = readListFromPref(this);
-
+        allSongs = readListFromPref(this,LIST_KEY);
         if (allSongs == null)
             scanMusic();
+        queue= readListFromPref(this,QUEUE_KEY);
+        if (queue == null) {
+            queue = (ArrayList<File>) allSongs.clone();
+            writeArrayListInPref(this,queue,QUEUE_KEY);
+        }
+
         adapter = new view_page_adapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        adapter.addFragments(new all_songs_fragment(allSongs), "All songs");
-        adapter.addFragments(new now_playing_fragment(allSongs), "Playing");
+        adapter.addFragments(new all_songs_fragment(allSongs,queue), "All songs");
+        adapter.addFragments(new now_playing_fragment(queue), "Playing");
         adapter.addFragments(new favourite_fragment(), "Favourites");
         view_pager.setAdapter(adapter);
 
